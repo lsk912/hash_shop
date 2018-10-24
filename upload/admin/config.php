9b5f5a3edc11da75ae89d55bed93f8a28cdb7820
Lussen <lsk@hashfax.com>
2018-10-24 16:31:26 +0800

    hashsh_shop

---
+++ upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.hash-shop.me/admin/');
+define('HTTP_CATALOG', 'http://www.hash-shop.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.hash-shop.me/admin/');
+define('HTTPS_CATALOG', 'http://www.hash-shop.me/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Lamp/www/hash-shop/upload/admin/');
+define('DIR_SYSTEM', 'E:/Lamp/www/hash-shop/upload/system/');
+define('DIR_IMAGE', 'E:/Lamp/www/hash-shop/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'E:/Lamp/www/hash-shop/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root123');
+define('DB_DATABASE', 'hash-shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ upload/system/storage/cache/32/32721b2c96d9683a54c687ef51da9bef90d323d0db0ee846e4d7b16c52031980.php
@@ -0,0 +1,51 @@
+<?php
+
+/* journal2/template/common/content_bottom.twig */
+class __TwigTemplate_e5c15242d343de21c11de12da61903fec7cb865f4e15e7a026a468397b9e1c66 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+            // line 2
+            echo "  ";
+            echo $context["module"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "journal2/template/common/content_bottom.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 2,  19 => 1,);
+    }
+}
+/* {% for module in modules %}*/
+/*   {{ module }}*/
+/* {% endfor %}*/
+/* */
